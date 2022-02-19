9ae6b2608f73caf25190ece3473be872456f506c
Jarrod Seccombe <jsec@hey.com>
2022-02-19 14:33:30 -0500

    Update: Add error handling middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,25 @@
+import { StatusCodes } from 'http-status-codes';
+import { isBoom } from '@hapi/boom';
+import Router from '@koa/router';
+import { Context, Next } from 'koa';
+
+const setErrorResponse = (ctx: Context, statusCode: number, message: string) => {
+  ctx.status = statusCode;
+  ctx.body = {
+    message,
+    correlationId: ctx.state.correlationId
+  } ;
+}
+
+export const errorHandler: Router.Middleware = async (ctx: Context, next: Next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (isBoom(err)) {
+      setErrorResponse(ctx, err.output.statusCode, err.message);
+    } else {
+      setErrorResponse(ctx, StatusCodes.INTERNAL_SERVER_ERROR, (err as Error).message);
+    }
+  }
+};
+
