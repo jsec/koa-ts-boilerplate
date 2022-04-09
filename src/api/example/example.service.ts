d87be2be20dd969e18289356c9aba8835c470c10
Jarrod Seccombe <jsec@hey.com>
2022-04-08 22:31:36 -0400

    Update: Add Option type to clean up service function return types

--- src/api/example/example.service.ts
+++ src/api/example/example.service.ts
@@ -1,11 +1,12 @@
+import { Option } from '../../core/types';
 import { Example } from '../../db/models/example.model';
 
 export class ExampleService {
-  public async getAll(): Promise<Example[] | undefined> {
+  public async getAll(): Promise<Option<Example[]>> {
     return Example.query();
   }
 
-  public async getById(id: number): Promise<Example | undefined> {
+  public async getById(id: number): Promise<Option<Example>> {
     return Example.query().where('id', id).first();
   }
 }
